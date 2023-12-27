9c33494224fd8a513e726c8229255878b5f62658
rsj <rseanjustice@gmail.com>
2023-12-27 07:55:15 -0700

    added number array validation

--- request/parameter/number_validation.go
+++ request/parameter/number_validation.go
@@ -1,6 +1,7 @@
 package parameter
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -30,4 +31,45 @@ func (p NumberValidator) Validate(num float64) error {
 		}
 	}
 	return fmt.Errorf("value [%f] not in %v", num, p.OneOf)
+}
+
+type NumberArrayValidator struct {
+	Values  []float64 `json:"values"`
+	Min     *float64  `json:"min"`
+	Max     *float64  `json:"max"`
+	Present []float64 `json:"present"`
+}
+
+func (n NumberArrayValidator) Validate(nums []float64) error {
+	if len(n.Values) > 0 {
+		if len(n.Values) != len(nums) {
+			return errors.New("validator values lenght must match values length")
+		}
+		for i := range n.Values {
+			if n.Values[i] != nums[i] {
+				return fmt.Errorf("value [%f] does not equal %f", nums[i], n.Values[i])
+			}
+		}
+	}
+	for _, num := range nums {
+		if n.Min != nil && num < *n.Min {
+			return fmt.Errorf("value [%f] is less than %f", num, *n.Min)
+		}
+		if n.Max != nil && num > *n.Max {
+			return fmt.Errorf("value [%f] is greater than %f", num, *n.Max)
+		}
+	}
+	if len(n.Present) == 0 {
+		return nil
+	}
+	nset := map[float64]struct{}{}
+	for _, n := range nums {
+		nset[n] = struct{}{}
+	}
+	for _, p := range n.Present {
+		if _, has := nset[p]; !has {
+			return fmt.Errorf("value [%f] not in %v", p, nums)
+		}
+	}
+	return nil
 }
--- request/parameter/number_validation_test.go
+++ request/parameter/number_validation_test.go
@@ -216,3 +216,184 @@ func TestParameterDataNumberValidation_Validate_OneOf(t *testing.T) {
 		})
 	}
 }
+
+func TestNumberArrayValidator_Validate_Values(t *testing.T) {
+	type fields struct {
+		Values []float64
+	}
+	type args struct {
+		nums []float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Values: []float64{12, 34, 98},
+			},
+			args: args{
+				nums: []float64{12, 34, 98},
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure: value",
+			fields: fields{
+				Values: []float64{12, 34, 98},
+			},
+			args: args{
+				nums: []float64{12, 111, 98},
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure: length",
+			fields: fields{
+				Values: []float64{12, 34, 98, 1},
+			},
+			args: args{
+				nums: []float64{12, 34, 98},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NumberArrayValidator{
+				Values: tt.fields.Values,
+			}
+			if err := n.Validate(tt.args.nums); (err != nil) != tt.wantErr {
+				t.Errorf("NumberArrayValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNumberArrayValidator_Validate_Min_Max(t *testing.T) {
+	type fields struct {
+		Min *float64
+		Max *float64
+	}
+	type args struct {
+		nums []float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Min: func() *float64 {
+					m := 10.0
+					return &m
+				}(),
+				Max: func() *float64 {
+					m := 52.0
+					return &m
+				}(),
+			},
+			args: args{
+				nums: []float64{11, 34, 51},
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure: min",
+			fields: fields{
+				Min: func() *float64 {
+					m := 10.0
+					return &m
+				}(),
+				Max: func() *float64 {
+					m := 52.0
+					return &m
+				}(),
+			},
+			args: args{
+				nums: []float64{11, 34, 7, 51},
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure: max",
+			fields: fields{
+				Min: func() *float64 {
+					m := 10.0
+					return &m
+				}(),
+				Max: func() *float64 {
+					m := 52.0
+					return &m
+				}(),
+			},
+			args: args{
+				nums: []float64{11, 34, 117, 51},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NumberArrayValidator{
+				Min: tt.fields.Min,
+				Max: tt.fields.Max,
+			}
+			if err := n.Validate(tt.args.nums); (err != nil) != tt.wantErr {
+				t.Errorf("NumberArrayValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNumberArrayValidator_Validate_Present(t *testing.T) {
+	type fields struct {
+		Present []float64
+	}
+	type args struct {
+		nums []float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Present: []float64{10, 34},
+			},
+			args: args{
+				nums: []float64{9, 22, 34, 76, 10},
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure",
+			fields: fields{
+				Present: []float64{10, 34},
+			},
+			args: args{
+				nums: []float64{9, 22, 5534, 76, 10},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NumberArrayValidator{
+				Present: tt.fields.Present,
+			}
+			if err := n.Validate(tt.args.nums); (err != nil) != tt.wantErr {
+				t.Errorf("NumberArrayValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
