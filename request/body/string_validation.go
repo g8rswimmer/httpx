f217dedf32baa4d5880af37287a3891c2251acd7
rsj <rseanjustice@gmail.com>
2023-12-28 16:25:36 -0700

    added body string validation

---
+++ request/body/string_validation.go
@@ -0,0 +1,42 @@
+package body
+
+import (
+	"fmt"
+
+	"github.com/g8rswimmer/httpx/request/parameter"
+)
+
+type StringValidator struct {
+	parameter.StringValidator
+}
+
+func (s StringValidator) Validate(value any) error {
+	str, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("value is not a string [%T]", value)
+	}
+	return s.StringValidator.Validate(str)
+}
+
+type StringArrayValidator struct {
+	parameter.StringArrayValidator
+}
+
+func (s StringArrayValidator) Validate(value any) error {
+	var strArr []string
+	switch arr := value.(type) {
+	case []any:
+		for _, v := range arr {
+			str, ok := v.(string)
+			if !ok {
+				return fmt.Errorf("value is not a string [%T]", v)
+			}
+			strArr = append(strArr, str)
+		}
+	case []string:
+		strArr = arr
+	default:
+		return fmt.Errorf("value is an array [%T]", arr)
+	}
+	return s.StringArrayValidator.Validate(strArr)
+}
