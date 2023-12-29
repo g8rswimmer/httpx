d1ad47c2ee935f57d9e98f1dab15363a74198d81
rsj <rseanjustice@gmail.com>
2023-12-28 17:09:15 -0700

    added body number validation

---
+++ request/body/number_validation.go
@@ -0,0 +1,43 @@
+package body
+
+import (
+	"fmt"
+
+	"github.com/g8rswimmer/httpx/request/parameter"
+)
+
+type NumberValidator struct {
+	parameter.NumberValidator
+}
+
+func (n NumberValidator) Validate(value any) error {
+	switch num := value.(type) {
+	case float64:
+		return n.NumberValidator.Validate(num)
+	default:
+		return fmt.Errorf("value is not a number [%T]", value)
+	}
+}
+
+type NumberArrayValidator struct {
+	parameter.NumberArrayValidator
+}
+
+func (n NumberArrayValidator) Validate(value any) error {
+	var numArr []float64
+	switch arr := value.(type) {
+	case []any:
+		for _, v := range arr {
+			n, ok := v.(float64)
+			if !ok {
+				return fmt.Errorf("value is not a number [%T]", v)
+			}
+			numArr = append(numArr, n)
+		}
+	case []float64:
+		numArr = arr
+	default:
+		return fmt.Errorf("value is not a number [%T]", value)
+	}
+	return n.NumberArrayValidator.Validate(numArr)
+}
---
+++ request/body/number_validation_test.go
@@ -0,0 +1,134 @@
+package body
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/g8rswimmer/httpx/request/parameter"
+)
+
+func TestNumberValidator_Validate(t *testing.T) {
+	type fields struct {
+		NumberValidator parameter.NumberValidator
+	}
+	type args struct {
+		value any
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				NumberValidator: parameter.NumberValidator{
+					Value: func() *float64 {
+						n := 42.0
+						return &n
+					}(),
+				},
+			},
+			args: args{
+				value: func() any {
+					str := `{"value":42}`
+					var obj map[string]any
+					_ = json.Unmarshal([]byte(str), &obj)
+					return obj["value"]
+				}(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure",
+			fields: fields{
+				NumberValidator: parameter.NumberValidator{
+					Value: func() *float64 {
+						n := 42.0
+						return &n
+					}(),
+				},
+			},
+			args: args{
+				value: func() any {
+					str := `{"value":"not a number"}`
+					var obj map[string]any
+					_ = json.Unmarshal([]byte(str), &obj)
+					return obj["value"]
+				}(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NumberValidator{
+				NumberValidator: tt.fields.NumberValidator,
+			}
+			if err := n.Validate(tt.args.value); (err != nil) != tt.wantErr {
+				t.Errorf("NumberValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNumberArrayValidator_Validate(t *testing.T) {
+	type fields struct {
+		NumberArrayValidator parameter.NumberArrayValidator
+	}
+	type args struct {
+		value any
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				NumberArrayValidator: parameter.NumberArrayValidator{
+					Values: []float64{65, 88},
+				},
+			},
+			args: args{
+				value: func() any {
+					str := `{"value":[65,88]}`
+					var obj map[string]any
+					_ = json.Unmarshal([]byte(str), &obj)
+					return obj["value"]
+				}(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "failure",
+			fields: fields{
+				NumberArrayValidator: parameter.NumberArrayValidator{
+					Values: []float64{65, 88},
+				},
+			},
+			args: args{
+				value: func() any {
+					str := `{"value":["sixt-five","eighty-eight"]}`
+					var obj map[string]any
+					_ = json.Unmarshal([]byte(str), &obj)
+					return obj["value"]
+				}(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NumberArrayValidator{
+				NumberArrayValidator: tt.fields.NumberArrayValidator,
+			}
+			if err := n.Validate(tt.args.value); (err != nil) != tt.wantErr {
+				t.Errorf("NumberArrayValidator.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
