261d759d832a37f401bf96558f95dcee81814922
rsj <rseanjustice@gmail.com>
2023-12-30 17:07:57 -0700

    added failing example

--- _examples/request/body/object/example.go
+++ _examples/request/body/object/example.go
@@ -23,6 +23,8 @@ func main() {
 	}
 
 	RequestJBodySuccess(schema)
+
+	RequestJBodyFail(schema)
 }
 
 func RequestJBodySuccess(schema jbody.Schema) {
@@ -57,4 +59,39 @@ func RequestJBodySuccess(schema jbody.Schema) {
 	}
 
 	fmt.Println("HTTP request body validated")
+}
+
+func RequestJBodyFail(schema jbody.Schema) {
+	b := `{
+		"id": "not an id",
+		"first_name": "Gary",
+		"last_name": "Doe",
+		"alias": ["Gar"],
+		"date_of_birth": "1999-05-15",
+		"home_address": {
+			"street_1": "123 Main St",
+			"city": "Springfield",
+			"state": "IL"
+		},
+		"other_addresses": [
+			{
+				"street_1": "123 Fun Way",
+				"city": "Springfield",
+				"state": "KY",
+				"zip": "41101"
+			}
+		],
+		"phone_number": "+16065551212",
+		"married": false,
+		"email": "gary.doe@gmail.com"
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "http://www.test.com/schema", strings.NewReader(b))
+
+	err := schema.Validate(req)
+	if err == nil {
+		panic(err)
+	}
+	fmt.Println(err.Error())
+	fmt.Println("HTTP request body validated: failed")
 }
